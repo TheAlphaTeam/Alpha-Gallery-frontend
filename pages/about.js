346648ed2449d458635d66a730de06c6236d23e3
Sewar-web <s.maqableh@yahoo.com>
2021-08-17 13:30:42 +0300

    about

---
+++ pages/about.js
@@ -0,0 +1,254 @@
+
+import Head from 'next/head'
+import React from 'react'
+export default function About() {
+
+
+
+
+    return (
+
+        <div className="">
+            <div className="relative block  " style={{ height: "300px" }}>
+
+                <div className="absolute w-full  h-full top-0 text-center items-center font-extrabold text-3xl" style={{ backgroundImage: `url('http://res2.weblium.site/res/5c1cd98f2914810022558981/5c223adfbd2020002629619e_optimized_1920')`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover' }} >
+                    <div className="bg-black w-full opacity-50  h-full absolute z-0">
+
+                    </div>
+                    <h1 className="text-align pt-28  text-black ">  About Alpha Gallery </h1>
+                </div>
+            </div>
+            <div className="" style={{ backgroundImage: `url('https://res2.weblium.site/res/5c74178873dbed00222cf694/5c75114c1947b80024e03da4_optimized_5120')`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover' }}>
+
+
+                <div className="flex flex-rows w-full  py-10   ">
+
+                    <div className="flex felx-cols   ">
+
+                        <div className="p-2">
+                            <img src="https://res2.weblium.site/res/5c1cd98f2914810022558981/5c223c62bd20200026296311_optimized" className="pb-2" />
+
+                            <img src="https://images.pexels.com/photos/2121666/pexels-photo-2121666.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" className=" pb-2" />
+                            <img src="https://res2.weblium.site/res/5c1cd98f2914810022558981/5c223c5ebd202000262962de_optimized" className="pb-2 " />
+
+                        </div>
+                        <div className="p-2">
+                            <img src="https://images.pexels.com/photos/2123337/pexels-photo-2123337.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=700 " className="pb-5" />
+
+                            <img src="https://images.pexels.com/photos/2372979/pexels-photo-2372979.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" className="" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <h3 className="align-middle text-center font-bold   font-sans capitalize animate-pulse pt-44 text-xl ">
+
+                            alpha gallery it's gallery  to show A virtualgallery
+                            that presentingcollection of artworks, information
+                            about exhibitions,Artest. Providinga platform for artists
+                            to present their works since it becomes hard while current
+                            conditions caused by the pandemic. </h3>
+                    </div>
+
+
+
+                </div>
+
+
+
+
+
+                <h1 className="text-5xl font-bold text-center pt-10 pb-5 font-sans "> Our Team</h1>
+
+
+
+
+
+                <div className="flex flex-row items-center justify-center flex-wrap  pt-10">
+                    <div className="flex flex-col items-center justify-center  p-1  h-96 w-96 ">
+                        <div className="inline-flex shadow-lg  rounded-full overflow-hidden h-60 w-60">
+                            <img src="https://avatars.githubusercontent.com/u/55560502?v=4"
+                                alt=""
+                                className="h-full w-full" />
+                        </div>
+
+                        <h2 className="mt-4 font-bold text-xl">Basel Atalla </h2>
+
+
+                        <h6 >Full Stack Developer</h6>
+
+                        <ul className="flex flex-row mt-4 space-x-2">
+                            <li>
+                                <a href=" https://github.com/baselatalla" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios/2x/github--v2.gif" className="rounded-full " />
+                                    <i className="fab fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/baselatalla" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios-filled/24/000000/facebook-circled--v2.png" />
+                                    <i className="fab fa-instagram "></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios-glyphs/30/000000/linkedin-circled--v2.png" />
+                                    <i className="fab fa-instagram "></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center  p-1 h-96 w-96   " >
+
+                        <div className="inline-flex shadow-lg  rounded-full overflow-hidden h-60 w-60">
+                            <img src="https://media-exp1.licdn.com/dms/image/C5603AQEUVv3Y5yW7YA/profile-displayphoto-shrink_200_200/0/1629112881896?e=1634774400&v=beta&t=eA_vDrqWKQqke7Id1787POzdnbXanAEAjUZxTdMRP9o" className="rounded-full"
+                                alt=""
+                                className="h-full w-full" />
+                        </div>
+
+                        <h2 className="mt-4 font-bold text-xl">Sewar Maqableh</h2>
+
+
+
+                        <h6 >Full Stack Developer</h6>
+
+
+                        <ul className="flex flex-row mt-4 space-x-2">
+                            <li>
+                                <a href=" https://github.com/Sewar-web" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios/2x/github--v2.gif" className="rounded-full bg" />
+                                    <i className="fab fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/sewar.ahmad.7798574/" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios-filled/24/000000/facebook-circled--v2.png" />
+                                    <i className="fab fa-instagram "></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios-glyphs/30/000000/linkedin-circled--v2.png" />
+                                    <i className="fab fa-instagram "></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center  p-1   h-96 w-96">
+                        <div className="inline-flex shadow-lg  rounded-full overflow-hidden h-60 w-60">
+                            <img src="https://avatars.githubusercontent.com/u/79087573?v=4" className="rounded-full"
+                                alt=""
+                                className="h-full w-full" />
+                        </div>
+
+                        <h2 className="mt-4 font-bold text-xl">Bayan Alkatib </h2>
+
+
+                        <h6 >Full Stack Developer</h6>
+
+                        <ul className="flex flex-row mt-4 space-x-2">
+                            <li>
+                                <a href=" https://github.com/bayan-alkhatib" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios/2x/github--v2.gif" className="rounded-full " />
+                                    <i className="fab fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios-filled/24/000000/facebook-circled--v2.png" />
+                                    <i className="fab fa-instagram "></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios-glyphs/30/000000/linkedin-circled--v2.png" />
+                                    <i className="fab fa-instagram "></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+
+                    <div className="flex flex-col items-center justify-center   h-96 w-96  ">
+                        <div className="inline-flex shadow-lg  rounded-full overflow-hidden h-60 w-60 ">
+                            <img src="https://avatars.githubusercontent.com/u/50866072?v=4"
+                                alt=""
+                                className="h-full w-full" />
+
+
+                        </div>
+                        <h2 className="mt-4 text-center font-bold text-xl space-x-2 ">Niveen Smadi </h2>
+                        <h6 >Full Stack Developer</h6>
+
+                        <ul className="flex flex-row mt-4 space-x-2">
+                            <li>
+                                <a href=" https://github.com/NiveenAlSmadi" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios/2x/github--v2.gif" className="rounded-full" />
+                                    <i className="fab fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://web.facebook.com/niveena.monem.9/" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios-filled/24/000000/facebook-circled--v2.png" />
+                                    <i className="fab fa-instagram "></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios-glyphs/30/000000/linkedin-circled--v2.png" />
+                                    <i className="fab fa-instagram "></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center h-96 w-96">
+                        <div className="inline-flex shadow-lg  rounded-full overflow-hidden h-60 w-60">
+                            <img src="https://avatars.githubusercontent.com/u/77916760?v=4"
+                                alt=""
+                                className="h-full w-full" />
+                        </div>
+
+                        <h2 className="mt-4 text-center font-bold text-xl">Layan Abo-shaweesh</h2>
+
+
+                        <h6 >Full Stack Developer</h6>
+
+                        <ul className="flex flex-row mt-4 space-x-2">
+                            <li>
+                                <a href=" https://github.com/layanabushaweesh" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios/2x/github--v2.gif" className="rounded-full" />
+                                    <i className="fab fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios-filled/24/000000/facebook-circled--v2.png" />
+                                    <i className="fab fa-instagram "></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo" className="flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800">
+                                    <img src="https://img.icons8.com/ios-glyphs/30/000000/linkedin-circled--v2.png" />
+                                    <i className="fab fa-instagram "></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+
+
+
+
+
+    )
+}
