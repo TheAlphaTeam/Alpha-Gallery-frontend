3636ee85000aa6a8b934dc2b0dfd095897b9d0a9
NiveenALSmadi <niveensmadi33@gmail.com>
2021-08-17 16:27:45 +0300

    add loading

---
+++ components/loading.js
@@ -0,0 +1,17 @@
+export default function Load() {
+    return (
+      <div class="spinner">
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    </div>
+    )
+
+
+}
+
+
+
+{/* <div className="pb-10 ">
+<Load/>
+</div> */}
