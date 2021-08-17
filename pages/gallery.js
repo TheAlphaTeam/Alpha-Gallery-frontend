8dfa946ca8cfec90840304684ba1a23439b76aea
NiveenALSmadi <niveensmadi33@gmail.com>
2021-08-17 21:26:45 +0300

    added gallery tamplate

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React from 'react';
+import ImageGallery from 'react-image-gallery';
+const images = [
+  {
+    original: 'https://www.metmuseum.org/-/media/images/art/collection-landing-page/your-collection/metpublications.jpg?as=1&la=en&mh=424&mw=672',
+    thumbnail: 'https://www.metmuseum.org/-/media/images/art/collection-landing-page/your-collection/metpublications.jpg?as=1&la=en&mh=224&mw=372',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
 
-export default function View(props){
-
-      return(
-     
+export default function View() {
+    
+    return (<ImageGallery items={images} />
       )
-
-}
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,64 @@
-import axios from "axios";
-import { useEffect, useState } from 'react'
-import LoginForm from '../components/login'
-import GalleryHome from '../components/GalleryHome';
+// import axios from "axios";
+// import { useEffect, useState } from 'react'
+// import LoginForm from '../components/login'
+// import GalleryHome from '../components/GalleryHome';
 
 
-const baseUrl= 'https://alphagallery.herokuapp.com/'
-const tokenUrl = baseUrl + 'api/token/'
+// const baseUrl= 'https://alphagallery.herokuapp.com/'
+// const tokenUrl = baseUrl + 'api/token/'
 
-export default function Home() {
+// export default function Home() {
 
-  const [token, setToken] = useState('');
-  const [storedToken, setStoredToken] = useState('')
-  const [storedUser, setStoredUser] = useState('')
-  const [storedpass, setStoredPass] = useState('')
-
-
-async function getToken(credentials){
-  const fetchedToken = await axios.post( tokenUrl, credentials);
-  setToken(fetchedToken.data.access)
-}
+//   const [token, setToken] = useState('');
+//   const [storedToken, setStoredToken] = useState('')
+//   const [storedUser, setStoredUser] = useState('')
+//   const [storedpass, setStoredPass] = useState('')
 
 
-function loginHandler(credentials){
-    getToken(credentials)
-    console.log('fun',token)
-}
+// async function getToken(credentials){
+//   const fetchedToken = await axios.post( tokenUrl, credentials);
+//   setToken(fetchedToken.data.access)
+// }
 
 
-useEffect(()=>{
-  setStoredPass(localStorage.getItem('password'))
-  setStoredUser(localStorage.getItem('username'))
-  setStoredToken(localStorage.getItem('token'))
-})
+// function loginHandler(credentials){
+//     getToken(credentials)
+//     console.log('fun',token)
+// }
+
+
+// useEffect(()=>{
+//   setStoredPass(localStorage.getItem('password'))
+//   setStoredUser(localStorage.getItem('username'))
+//   setStoredToken(localStorage.getItem('token'))
+// })
    
-useEffect(()=>{
-  if (!localStorage.getItem('token')){
-    localStorage.setItem('token',token)
-  }
-})
+// useEffect(()=>{
+//   if (!localStorage.getItem('token')){
+//     localStorage.setItem('token',token)
+//   }
+// })
 
  
 
-  if (token || storedToken ){
-    return (
-      <GalleryHome storedToken={storedToken} />
+//   if (token || storedToken ){
+//     return (
+//       <GalleryHome storedToken={storedToken} />
 
-    )
-  }
+//     )
+//   }
   
-  if (!storedToken && !token){
-    return(
-  <LoginForm loginHandler={loginHandler}  token={token}/>
-    )
-  }
+//   if (!storedToken && !token){
+//     return(
+//   <LoginForm loginHandler={loginHandler}  token={token}/>
+//     )
+//   }
+// }
+
+import View from "./gallery.js"
+export default function Home() {
+  return(
+    <View/>
+  )
+
 }
-
-
