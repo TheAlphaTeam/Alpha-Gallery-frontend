f355ede94a474232976ee50026e7f6e3ac7daf0f
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-08-18 15:29:06 +0300

    complete signout functionality

--- pages/logout.js
+++ pages/logout.js
@@ -1,24 +1,28 @@
 import Home from './index'
 import axios from "axios";
 import { useEffect, useState } from 'react'
-import LoginForm from '../components/login'
-import HomePage  from "./home";
-import { route } from 'next/dist/server/router';
-import Link from 'next/dist/client/link';
-import React from 'react'
 import {  Route } from 'next/router'
-
+import { useHistory } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+import { useRouter } from 'next/router'
+import Load from "../components/loading"
 
 
 export default function LogOut(props){
-  
+
+
+   const router = useRouter()
+    
     useEffect(()=>{
         localStorage.setItem('username','');
         localStorage.setItem('password','')
         localStorage.setItem('token','')
+        router.push('/')
     })
 
-    return(
-        <Home/>
-    );
+    return (
+        <div className="pb-10 ">
+            <Load/>
+        </div>   
+    )
 }
